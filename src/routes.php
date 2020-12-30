7ae0e0b1398b54287db58eedfb63bfde77a2d989
Francis Fueconcillo <francisfueconcillo@gmail.com>
2020-12-30 23:32:54 +0900

    Fix sample controller

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/samplepurchase/{id}', 'PepperTech\LaraPaymongo\Controllers\SamplePurchaseController');
+Route::get('/samplepurchase/{id}', 'PepperTech\LaraPaymongo\SamplePurchaseController');
